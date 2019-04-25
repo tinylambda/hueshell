2f8622a88602a93cdcb231cbbbf458fdd532b370
Felix <tinylambda@gmail.com>
2019-04-25 17:46:49 +0800

    remove tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 
 setuptools.setup(
-    packages=setuptools.find_packages(exclude=['tests*']),
+    packages=setuptools.find_packages(exclude=["hueshell/tests"]),
     package_data={"": ["hueshell/.hue.ini"]},
     include_package_data=True,
     install_requires=["requests"],
