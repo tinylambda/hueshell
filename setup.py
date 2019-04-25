3c839700a71d72365da75dec21de0ca8aac58a6f
Felix <tinylambda@gmail.com>
2019-04-25 17:43:56 +0800

    Add package data

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import setuptools
 
 
 setuptools.setup(
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['tests*']),
+    package_data={"": ["hueshell/.hue.ini"]},
+    include_package_data=True,
     install_requires=["requests"],
 )
