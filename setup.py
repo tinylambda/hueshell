066b963c4a0a56e12a5d42ef1051271bcfa1925c
Felix <tinylambda@gmail.com>
2019-04-25 17:51:08 +0800

    setup bug fix

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 
 setuptools.setup(
-    packages=setuptools.find_packages(exclude=["hueshell/tests"]),
-    package_data={"": ["hueshell/.hue.ini"]},
+    packages=setuptools.find_packages(exclude=["tests"]),
+    package_data={"": [".hue.ini"]},
     include_package_data=True,
     install_requires=["requests"],
 )
